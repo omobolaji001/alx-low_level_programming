451947eb0c8c3fb9d41f9b0802f74f68f6beadc4
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-04-05 03:32:18 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,9 +19,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index != 0)
 	{
-		for (i = 0; i < index - 1 && prev != NULL; i++)	
+		for (i = 0; i < index - 1 && prev != NULL; i++)
 			prev = prev->next;
-		
 	}
 
 	if (prev == NULL || (prev->next == NULL && index != 0))
