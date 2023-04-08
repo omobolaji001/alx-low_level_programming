df1c878d26a1cc5d79f207bac46a36bff8ac719a
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-04-07 18:05:14 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates file.
+ * @filename: the name of the file to be created.
+ * @text_content: string to write to the file.
+ *
+ * Return: returns 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t wrt;
+	int file_d;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_d = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 00600);
+
+	if (file_d == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = " ";
+
+	while (text_content[i] != '\0')
+	{
+		wrt = write(file_d, text_content, 1);
+		i++;
+	}
+
+	if (wrt == -1)
+		return (-1);
+
+	return (1);
+}
