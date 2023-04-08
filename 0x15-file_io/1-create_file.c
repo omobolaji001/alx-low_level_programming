2bcba45607d4fd63853889154c23a52b3df69d38
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-04-08 03:15:17 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int wrt;
+	ssize_t wrt;
 	int file_d;
-	int i = 0;
+	int i;
 
 	if (filename == NULL)
 		return (-1);
@@ -25,11 +25,11 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content[i] != '\0')
-	{
-		wrt = write(file_d, text_content, 1);
-		i++;
-	}
+	for (i = 0; text_content[i]; i++)
+		;
+
+	wrt = write(file_d, text_content, 1);
+	
 
 	if (wrt == -1)
 		return (-1);
