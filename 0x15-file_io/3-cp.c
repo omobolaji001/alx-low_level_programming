d96fa440e23bed7dfa6cd89d923b431d21faa4ab
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-04-07 19:22:47 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,10 +30,10 @@ void error_file(int file_from, int file_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars;
-	ssize_t nwr;
-	char buf[1024];
+	int file_from, file_to, error_close;
+	ssize_t rd;
+	ssize_t wrt;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -45,31 +45,31 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
+	rd = 1024;
 
-	while (nchars == 1024)
+	while (rd == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		rd = read(file_from, buf, 1024);
+		if (rd == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		wrt = write(file_to, buf, rd);
+		if (wrt == -1)
 			error_file(0, -1, argv);
 	}
 
-	err_close = close(file_from);
+	error_close = close(file_from);
 
-	if (err_close == -1)
+	if (error_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(file_to);
+	error_close = close(file_to);
 
-	if (err_close == -1)
+	if (error_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
