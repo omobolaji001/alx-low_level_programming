c4600deb303d210e51ffd75cbbadbd553c5325a4
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-04-14 03:15:29 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,16 +11,16 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int wrt;
-        int file_d;
-        int i;
+	int file_d;
+	int i;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        file_d = open(filename, O_WRONLY |O_APPEND);
+	file_d = open(filename, O_WRONLY |O_APPEND);
 
-        if (file_d == -1)
-                return (-1);
+	if (file_d == -1)
+		return (-1);
 	else
 		return (1);
 
@@ -28,14 +28,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i]; i++)
 			;
-        	
+		
 		wrt = write(file_d, text_content, i);
 
-        	if (wrt == -1)
-                	return (-1);
+		if (wrt == -1)
+			return (-1);
 	}
 
-        close(file_d);
+	close(file_d);
 
         return (1);
 }
