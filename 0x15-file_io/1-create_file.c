2ce9092b783c63a5de9c26da731183748dd2c046
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-04-14 03:08:46 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 00600);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
 
 	if (file_d == -1)
 		return (-1);
